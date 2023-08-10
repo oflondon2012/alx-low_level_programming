c614785fa0916148c5666f2e1af4979badcd004a
oflondon2012 <gtadebayo2023@gmail.com>
2023-08-10 13:43:56 -0700

    Program that print a string with printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - Program that a line of string follow by newline usin
+ * main - Program that a line of string follow by newline using printf function
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
