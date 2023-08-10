367b9e38858d9d89d899c67feb6126a8b07aab1c
oflondon2012 <gtadebayo2023@gmail.com>
2023-08-10 13:31:30 -0700

    Program that print a string with puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Program that a line of string follow by newline usin
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
