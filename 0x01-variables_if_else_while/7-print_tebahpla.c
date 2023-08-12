f21d17e9bf26d2d9d4848472994891ac1935aff8
oflondon2012 <gtadebayo2023@gmail.com>
2023-08-11 23:43:31 -0700

    Program that print lower case in reserce order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Program that print loer case alphabeth in reverse order
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	char j;
+
+	j = 'z';
+	while
+		(j >= 'a')
+		{
+			putchar(j);
+			--j;
+		}
+	puchar('\n');
+	return (0);
+}
