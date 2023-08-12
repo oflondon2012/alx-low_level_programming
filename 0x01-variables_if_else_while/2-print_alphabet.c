d77ab7e7262d029aeceba400530e70a1d7b3de06
oflondon2012 <gtadebayo2023@gmail.com>
2023-08-11 21:12:24 -0700

    Program that print the alphabeth

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Program that print the alphabeth
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char j;
+
+	j = 'a';
+	while
+		(j <= 'z')
+		{
+			putchar(j);
+			j++;
+		}
+	putchar('\n');
+	return (0);
+}
