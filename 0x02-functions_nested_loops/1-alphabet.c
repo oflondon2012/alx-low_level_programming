bd2e32e56edd9f28818460d1ebaa7c71778c02ec
oflondon2012 <gtadebayo2023@gmail.com>
2023-08-15 16:49:56 -0700

    Function to print alphabeth in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Function that print the alphabeth in lowercase
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
