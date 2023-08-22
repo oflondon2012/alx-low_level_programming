34f35bd080324ce719d309738fcb76617731378e
oflondon2012 <gtadebayo2023@gmail.com>
2023-08-22 15:31:21 -0700

    program that computes and prints the sum of all the multiples of 3 or 5 below 1024

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_times_table - function to print times table of the given input
+ *
+ * @n: The given number to compute
+ *
+ */
+
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	if (n >= 0 && n <= 15)
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
+			for (j = 1; j <= n; j++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				k = i * j;
+				if (k >= 100)
+				{
+					_putchar((k / 100) + '0');
+					_putchar(((k / 100) % 10) + '0');
+				}
+				else if (k <= 99 && k >= 10)
+				{
+					_putchar((k / 10) + '0');
+				}
+				else if (k <= 99)
+				{
+					_putchar(' ');
+				}
+				else if (k <= 9)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+}
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Program that list all natural number below 1024
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int j = 0, nsum = 0;
+
+	while (j < 1024)
+	{
+		if ((j % 3) == 0 || (j % 5) == 0)
+		{
+			nsum = nsum + j;
+		}
+		j++,
+	}
+	printf("%\n", nsum);
+	return (0);
+}
