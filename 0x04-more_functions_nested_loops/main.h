94acb0c85176997c66a6925c9cf2bec60e65567b
oflondon2012 <gtadebayo2023@gmail.com>
2023-08-21 22:07:42 -0700

    function that checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char j);
+int _isupper(int j);
+int isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_number(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif
