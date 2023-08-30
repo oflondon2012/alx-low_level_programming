91424ca76a32cec73d8e344115098be1b70ac24e
oflondon2012 <gtadebayo2023@gmail.com>
2023-08-29 19:31:27 -0700

    function that print number from 0 to 9 apart from 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - function that print number from 0 to 9 
+ * print_most_numbers - function that print number from 0 to 9
  * apart from 2 and 4
  *
  * Return: always 0 (success)
