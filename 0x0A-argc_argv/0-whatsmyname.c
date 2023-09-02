4bb9bea504eeef832bd59a913e4928eec2aab937
oflondon2012 <gtadebayo2023@gmail.com>
2023-09-02 05:11:24 +0000

    function that print it name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,7 +8,7 @@
  * Return: Always 0 (success)
  */
 
-int main(int agrc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAN_H
+
+
+
+#endif
