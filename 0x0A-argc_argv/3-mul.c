2bec5ba402d2d84617eb380b399b3003c7f3465e
oflondon2012 <gtadebayo2023@gmail.com>
2023-09-02 07:44:15 +0000

    program that multiplies two number in command argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - program that multiply two numbers in argument
@@ -15,6 +16,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", _atoi(argv[1]) * _atoi(argv[2]));
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
