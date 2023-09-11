94407b30c36264fcb5139947dc6612681166af15
oflondon2012 <gtadebayo2023@gmail.com>
2023-09-11 12:33:24 -0700

    Dog structure information

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Readme for structure and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure that create dog information
+ * @nme: name of diff dog
+ * @age: variable for age
+ * @owner: owner info
+ *
+ * Desc: learning to write structure
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
