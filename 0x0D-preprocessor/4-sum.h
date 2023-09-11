60e14fdc7c686e268e4ef4e86119b014c02de899
oflondon2012 <gtadebayo2023@gmail.com>
2023-09-11 11:26:39 -0700

    function-like macro SUM(x, y) that computes the sum of the numbers

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef MACROSUM_H
+#define MACROSUM_H
+
+/**
+ * File: 4-sum.h
+ *
+ * @x: first value to comput
+ * @y: second value for computation
+ * Desc: Function that computes the sum of the given number
+ */
+
+#define SUM(x, y) (x + y)
+
+#endif
