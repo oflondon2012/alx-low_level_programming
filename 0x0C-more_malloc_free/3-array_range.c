ab4bb69d4fb9305770b30127d1ad066bda95803f
oflondon2012 <gtadebayo2023@gmail.com>
2023-09-13 21:01:31 +0000

    function to comppute array range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+/**
+ * array_range - function that create array integer value
+ * @min: minumum intger
+ * @max: maximum value
+ *
+ * Return: a pointer to the newwly create
+ */
+
+int *array_ramge
