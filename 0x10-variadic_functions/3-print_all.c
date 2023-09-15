b1fca5acdd85ebc9b12ddc838b244c73bf028606
oflondon2012 <gtadebayo2023@gmail.com>
2023-09-15 03:06:37 +0000

    All variadic functions task

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,50 +1,45 @@
 #include "variadic_functions.h"
-
 /**
  * print_all - function that prints anything
  * @format: list of augument type
- *
  * Return: void
  */
 
 void print_all(const char * const format, ...)
 {
 	char *stg;
-	int j;
-	char *separator;
+	int j = 0;
+	char *sepa = "";
+	va_list pall;
 
-	va_list printall;
-
-	va_start(printall, format);
+	va_start(pall, format);
 
 	if (format)
 	{
-		separator = "";
-		j = 0;
 		while (format[j])
 		{
 			switch (format[j])
 			{
 				case 'i':
-					printf("%s%d", separator, va_arg(printall, int));
+					printf("%s%d", sepa, va_arg(pall, int));
 				break;
 				case 'c':
-					printf("%s%c", separator, va_arg(printall, int));
+					printf("%s%c", sepa, va_arg(pall, int));
 				break;
 				case 'f':
-					printf("%s%f", separator, va_arg(printall, double));
+					printf("%s%f", sepa, va_arg(pall, double));
 				break;
 				case 's':
-					stg = va_arg(printall, char *);
+					stg = va_arg(pall, char *);
 					if (stg == NULL)
 						stg = "nil";
-					printf("%s%s", separator, stg);
+					printf("%s%s", sepa, stg);
 				break;
 				default:
 					j++;
 					continue;
 			}
-			separator = ",";
+			sepa = ",";
 			j++;
 		}
 	}
