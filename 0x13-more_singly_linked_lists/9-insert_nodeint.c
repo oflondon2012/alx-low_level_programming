b99cb49788bc7dc44c70ad3f34526e129f867592
oflondon2012 <gtadebayo2023@gmail.com>
2023-09-27 23:32:32 +0000

    function to add new node at any given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *node, *add = *head;
-	unsigned int j;
+	unsigned int j = 0;
 
 	node = malloc(sizeof(listint_t));
 	if (!node || !head)
@@ -34,9 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (node);
 		}
 		else
-		{
 			add = add->next;
-		}
 		j++;
 	}
 	return (NULL);
