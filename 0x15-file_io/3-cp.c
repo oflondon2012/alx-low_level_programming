e7c6f1765263d0f82d7137241cf94bda3ef4135b
oflondon2012 <gtadebayo2023@gmail.com>
2023-10-06 21:53:54 +0000

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 void error_handling_file(int file_from, int file_to, char *argv[]);
+
 /**
  * main - Program that copies the content of a file to another file
  * @argc: count of argument
@@ -11,39 +12,40 @@ void error_handling_file(int file_from, int file_to, char *argv[]);
 
 int main(int argc, char *argv[])
 {
-	int file_from, file_to;
+	int file_from, file_to, close_error;
 	char buff[1024];
-	ssize_t cha = 1024, sdw;
+	ssize_t sdw, cha;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		fprintf(stderr, "Usage: %s file_from file_to\n", argv[0]);
+		exit(99);
 	}
 	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	error_handling_file(file_from, file_to, argv);
-	while (cha == 1024)
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	if (file_from == -1 || file_to == -1)
+		error_handling_file(file_from, file_to, argv);
+	cha = 0;
+	while ((cha = read(file_from, buff, sizeof(buff))) >= 0)
 	{
-		cha = read(file_from, buff, 1024);
 		if (cha == -1)
 			error_handling_file(-1, 0, argv);
-		sdw = write(file_to, buff, 1024);
+		sdw = write(file_to, buff, cha);
 		if (sdw == -1)
 			error_handling_file(0, -1, argv);
+		if (cha == 0)
+			break;
 	}
-	if (close(file_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
-	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
+
+	close_error = close(file_from);
+	if (close_error == -1)
+		error_handling_file(-1, 0, argv);
+	close_error = close(file_to);
+	if (close_error == -1)
+		error_handling_file(0, -1, argv);
 	return (0);
 }
+
 /**
  * error_handling_file - function that check if a file can
  * be opened or closed and handle errors
@@ -57,12 +59,12 @@ void error_handling_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+		fprintf(stderr, "Error: Can't write to file %s\n", argv[2]);
 		exit(99);
 	}
 }
