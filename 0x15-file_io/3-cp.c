b30ba311a04c90c761e4238f57901f6bb4d8bcd4
oflondon2012 <gtadebayo2023@gmail.com>
2023-10-07 01:54:53 +0000

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
-void error_handling_file(int file_from, int file_to, char *argv[]);
+
+void error_handling(const char *message, const char *filename, int exit_code);
 /**
  * main - Program that copies the content of a file to another file
  * @argc: count of argument
@@ -9,59 +10,65 @@ void error_handling_file(int file_from, int file_to, char *argv[]);
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to;
-	char buff[1024];
-	ssize_t sdw, cha = 0;
+	const char *file_from = argv[1], *file_to = argv[2];
+	int fd_from, fd_to;
+	char buffer[1024];
+	ssize_t bytes_read, bytes_written;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	if (file_from == -1 || file_to == -1)
-		error_handling_file(file_from, file_to, argv);
-	while ((cha = read(file_from, buff, sizeof(buff))) >= 0)
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
 	{
-		if (cha == -1)
-			error_handling_file(-1, 0, argv);
-		sdw = write(file_to, buff, cha);
-		if (sdw == -1)
-			error_handling_file(0, -1, argv);
-		if (cha == 0)
-			break;
+		error_handling("Error: Can't read from file", file_from, 98);
 	}
-	if (close(file_from) == -1)
+	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read fd %d\n", file_from);
-		exit(100);
+		error_handling("Error: Can't write to file", file_to, 99);
 	}
-	if (close(file_to) == -1)
+	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			error_handling("Error: Can't write to file", file_to, 99);
+		}
 	}
+
+	if (bytes_read == -1)
+	{
+		error_handling("Error: Can't read from file", file_from, 98);
+	}
+
+	if (close(fd_from) == -1 || close(fd_to) == -1)
+	{
+		error_handling("Error: Can't close fd", "", 100);
+	}
+
 	return (0);
 }
+
 /**
- * error_handling_file - function that check if a file can open or close
- * @file_from: pointing to name of file
- * @file_to: either close  or open
- * @argv: vector argument
+ * error_handling - function that check if a file can open or close
+ * @message: pointing to name of file
+ * @filename: either close  or open
+ * @exit_code: vector argument
  *
  * Return: void
  */
-void error_handling_file(int file_from, int file_to, char *argv[])
+void error_handling(const char *message, const char *filename, int exit_code)
 {
-	if (file_from == -1)
+	if (filename[0] != '\0')
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		dprintf(STDERR_FILENO, "%s %s\n", message, filename);
 	}
-	if (file_to == -1)
+	else
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "%s\n", message);
 	}
+	exit(exit_code);
 }
--- 0x15-file_io/4-cp.c
+++ 0x15-file_io/4-cp.c
@@ -2,10 +2,53 @@
 #include <stdio.h>
 void error_handling_file(int file_from, int file_to, char *argv[]);
 /**
- * error_handling_file - function that check if a file open or close
+ * main - Program that copies the content of a file to another file
+ * @argc: count of argument
+ * @argv: vector of argument
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to;
+	char buff[1024];
+	ssize_t sdw, cha = 0;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	if (file_from == -1 || file_to == -1)
+		error_handling_file(file_from, file_to, argv);
+	while ((cha = read(file_from, buff, sizeof(buff))) >= 0)
+	{
+		if (cha == -1)
+			error_handling_file(-1, 0, argv);
+		sdw = write(file_to, buff, cha);
+		if (sdw == -1)
+			error_handling_file(0, -1, argv);
+		if (cha == 0)
+			break;
+	}
+	if (close(file_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read fd %d\n", file_from);
+		exit(100);
+	}
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+	return (0);
+}
+/**
+ * error_handling_file - function that check if a file can open or close
  * @file_from: pointing to name of file
  * @file_to: either close  or open
- * @argv: vector argumentgcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-cp.c -o cp
+ * @argv: vector argument
  *
  * Return: void
  */
@@ -21,48 +64,4 @@ void error_handling_file(int file_from, int file_to, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 		exit(99);
 	}
-}
-/**
- * main - Program that copies the content of a file to another file
- * @argc: count of argument
- * @argv: vector of argument
- *
- * Return: 0 if successful
- */
-int main(int argc, char *argv[])
-{
-	int file_from, file_to, close_error;
-	char buff[1024];
-	ssize_t sdw, cha;
-
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-		exit(99);
-	}
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_handling_file(file_from, file_to, argv);
-	cha = 0;
-	while ((cha = read(file_from, buff, sizeof(buff))) > 0)
-	{
-		if (cha == -1)
-			error_handling_file(-1, 0, argv);
-		sdw = write(file_to, buff, cha);
-		if (sdw == -1)
-			error_handling_file(0, -1, argv);
-	}
-	close_error = close(file_from);
-	if (close_error == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
-	close_error = close(file_to);
-	if (close_error == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
-	return (0);
 }
