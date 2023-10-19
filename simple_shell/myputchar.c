d49a2534c3b887902f883d79e8d12d09f5edec49
oflondon2012 <gtadebayo2023@gmail.com>
2023-10-19 03:14:27 +0000

    The simple shell task in alx

---
+++ simple_shell/myputchar.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * ge_myputchar - function that write the character to standard out
+ * @strg: represent the string to print out
+ *
+ * Return: Always 1 on success
+ */
+
+int ge_myputchar(char strg)
+{
+	return (write(1, &strg, 1));
+}
+
+/**
+ * ge_puts - using puts function to print out
+ * @strg: string to print out
+ *
+ * Return: void:
+ */
+void ge_puts(char *strg)
+{
+	int j;
+
+	for (j = 0; strg[j]; j++)
+	{
+		ge_myputchar(strg[j]);
+	}
+}
+
