d5185e5e1dc6cd06567b375f9f766ada478e2b7d
oflondon2012 <gtadebayo2023@gmail.com>
2023-11-18 09:23:32 +0000

    updating the task

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -8,74 +8,184 @@ void print_error(const char *message)
 	fprintf(stderr, "%s\n", message);
 	exit(98);
 }
+/**
+ * print_class - function to print elf message
+ * @elf_class: specifies the architecture for which the binaty is intended
+ */
+void print_class(unsigned char elf_class)
+{
+	printf("  Class:                             ");
+	switch (elf_class)
+	{
+		case ELFCLASSNONE:
+			printf("None\n");
+			break;
+		case ELFCLASS32:
+			printf("ELF32\n");
+			break;
+		case ELFCLASS64:
+			printf("ELF64\n");
+			break;
+		default:
+			printf("Unknown (%02x)\n", elf_class);
+			break;
+	}
+}
 
 /**
- * print_elf_header_info - function that print eld header
- * @header:pointer to the heder to print
- *
- * Return: void
+ * print_data - Function to print data encoding
+ * @data_encoding: determines the endianness of the data in the ELF file
  */
-void print_elf_header_info(Elf64_Ehdr *header)
+void print_data(unsigned char data_encoding)
 {
-	char *class_str, *data_str, *abi_str;
-	int i;
+	printf("  Data:                              ");
+	switch (data_encoding)
+	{
+		case ELFDATANONE:
+			printf("None\n");
+			break;
+		case ELFDATA2LSB:
+			printf("2's complement, little endian\n");
+			break;
+		case ELFDATA2MSB:
+			printf("2's complement, big endian\n");
+			break;
+		default:
+			printf("Unknown (%02x)\n", data_encoding);
+			break;
+	}
+}
 
+/**
+ * print_version - Function to print version
+ * @version: indicates the version of the ELF specification
+ */
+void print_version(Elf32_Word version)
+{
+	printf("  Version:                           %d\n", version);
+}
+
+/**
+ * print_osabi - Function to print OS/ABI
+ * @osabi: specifies the target environment and system calls that
+ *
+ */
+void print_osabi(unsigned char osabi)
+{
+	printf("  OS/ABI:                            ");
+	switch (osabi)
+	{
+		case ELFOSABI_NONE:
+			printf("UNIX - System V\n");
+			break;
+		case ELFOSABI_HPUX:
+			printf("UNIX - HP-UX\n");
+			break;
+		case ELFOSABI_NETBSD:
+			printf("UNIX - NetBSD\n");
+			break;
+		default:
+			printf("Unknown (%02x)\n", osabi);
+			break;
+	}
+}
+
+/**
+ * print_abi_version - Function to print ABI version
+ * @abi_version: indicates the specific version of ABI that
+ * the ELF is designed to work with
+ */
+void print_abi_version(unsigned char abi_version)
+{
+	printf("  ABI Version:                       %d\n", abi_version);
+}
+
+/**
+ * print_file_type - Function to print file type
+ * @file_type: specifies the file type
+ */
+void print_file_type(Elf32_Half file_type)
+{
+	printf("  Type:                              ");
+	switch (file_type)
+	{
+		case ET_NONE:
+			printf("None\n");
+			break;
+		case ET_REL:
+			printf("Relocatable\n");
+			break;
+		case ET_EXEC:
+			printf("Executable\n");
+			break;
+		case ET_DYN:
+			printf("Shared object\n");
+			break;
+		case ET_CORE:
+			printf("Core dump\n");
+			break;
+		default:
+			printf("Unknown (%04x)\n", file_type);
+			break;
+	}
+}
+
+/**
+ * print_entry_point - Function to print entry point address
+ * @entry_point: indicates the entry point
+ */
+void print_entry_point(Elf32_Addr entry_point)
+{
+	printf("  Entry point address:               0x%lx\n",
+			(unsigned long)entry_point);
+}
+
+/**
+ * display_elf_header - Function to display ELF header information
+ * @filename: indicates the filename
+ */
+void display_elf_header(const char *filename)
+{
+	Elf64_Ehdr header;
+	int i, fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		print_error("Failed to open file");
+
+	if (read(fd, &header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
+		print_error("Failed to read ELF header");
+
+	printf("ELF Header:\n");
 	printf("  Magic:   ");
 	for (i = 0; i < EI_NIDENT; i++)
-	{
-		printf("%02x", header->e_ident[i]);
-		if (i < EI_NIDENT - 1)
-			printf(" ");
-	}
+		printf("%02x ", header.e_ident[i]);
 	printf("\n");
 
-	class_str = (header->e_ident[EI_CLASS] == ELFCLASS64) ? "ELF64" :
-	"ELF32";
-	data_str = (header->e_ident[EI_DATA] == ELFDATA2LSB) ?
-	"2's complement, little-endian" : "2's complement, big-endian";
-	abi_str = (header->e_ident[EI_OSABI] == ELFOSABI_SYSV) ? "UNIX - System V" :
-	"UNIX - NetBSD";
+	print_class(header.e_ident[EI_CLASS]);
+	print_data(header.e_ident[EI_DATA]);
+	print_version(header.e_version);
+	print_osabi(header.e_ident[EI_OSABI]);
+	print_abi_version(header.e_ident[EI_ABIVERSION]);
+	print_file_type(header.e_type);
+	print_entry_point(header.e_entry);
 
-	printf("  Class:                             %s\n", class_str);
-	printf("  Data:	                             %s\n", data_str);
-	printf("  Version:                           %d (current)\n",
-			header->e_ident[EI_VERSION]);
-	printf("  OS/ABI:                              %s\n", abi_str);
-	printf("  ABI Version:                              %d\n",
-			header->e_ident[EI_ABIVERSION]);
-	printf("  Type:	                              %s\n",
-			(header->e_type == ET_EXEC) ?
-			"EXEC (Executable file)" :
-			(header->e_type == ET_DYN) ? "DYN (Shared object file)" :
-			(header->e_type == ET_REL) ? "REL (Relocatable file)" : "UNIX - NetBSD");
-	printf("  Entry point address:                0x%lx\n", header->e_entry);
+	close(fd);
 }
+
 /**
- * main - main function
- * @argc: number of command line argu
- * @argv: array of command line argu
- *
- * Return: Always (0)
+ * main - Main function
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ * Return: 0 success
  */
 int main(int argc, char *argv[])
 {
-	int fd;
-	Elf64_Ehdr header;
-
 	if (argc != 2)
-		print_error("Usage: elf_header elf_filename");
+	{
+		fprintf(stderr, "Usage: %s <ELF file>\n", argv[0]);
+		return (98);
+	}
 
-	fd = open(argv[1], O_RDONLY);
-
-	if (fd == -1)
-		print_error("Error: Can't open file");
-	if (read(fd, &header, sizeof(header)) != sizeof(header) ||
-	memcmp(header.e_ident, ELFMAG, SELFMAG) != 0)
-		print_error("Error: Not an ELF file");
-
-	print_elf_header_info(&header);
-	close(fd);
-
+	display_elf_header(argv[1]);
 	return (0);
 }
-
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,5 +15,15 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void copy_file(int file_from, int file_to, char *argv[]);
 
+void print_error(const char *message);
+void print_class(unsigned char elf_class);
+void print_data(unsigned char data_encoding);
+void print_version(Elf32_Word version);
+void print_osabi(unsigned char osabi);
+void print_abi_version(unsigned char abi_version);
+void print_file_type(Elf32_Half file_type);
+void print_entry_point(Elf32_Addr entry_point);
+void display_elf_header(const char *filename);
+int main(int argc, char *argv[]);
 
 #endif
