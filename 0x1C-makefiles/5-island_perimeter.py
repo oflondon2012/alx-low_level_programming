09b90d27c168012f11fd887494b2ce651899552b
oflondon2012 <gtadebayo2023@gmail.com>
2024-02-22 03:36:00 +0000

    make files task

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Create a function def island_perimeter(grid): that returns the perimeter
+island described in grid
+"""
+
+
+def island_perimeter(grid):
+    """
+    This is the function for the task
+    """
+    pass
