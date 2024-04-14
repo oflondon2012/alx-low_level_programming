9f0cd820ea039909e3991b24bea32f3634161c15
oflondon2012 <gtadebayo2023@gmail.com>
2024-04-14 11:59:29 +0000

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,7 +2,7 @@
 
 /**
  * linear_search -function that searches for a value in an array of
- * 		integers using the Linear search algorithm
+ * integers using the Linear search algorithm
  * @array: pointer to the first element of the array to search.
  * @size: number of elements in the array.
  * @value: value to search for.
